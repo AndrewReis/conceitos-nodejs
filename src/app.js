b48069018fde72cf6e5980b3f9ba595636d9462c
AndrewReis <andrew-reiis@outlook.com>
2020-08-19 21:38:14 -0300

    rotas feitas: get, post, delete, post/likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+   response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+
+    const newRepository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+
+    repositories.push(newRepository);
+
+    return response.json(newRepository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id",(request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;  
+
+  const indexRepo = repositories.findIndex( repo => repo.id === id );
+
+  if(indexRepo < 0 ){
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  
+  const updateRepo = {
+    title,
+    url,
+    techs,
+  }
+
+  repositories[indexRepo] = updateRepo;
+
+  return response.send();
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const indexRepo = repositories.findIndex( repo => repo.id === id );
+
+    if(indexRepo < 0){
+      return response.status(400).json({ error: 'Repository not found.' });
+    }
+
+    repositories.splice(indexRepo, 1);
+
+    return response.status(204).send();
+
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",(request, response) => {
+    const { id } = request.params;
+   
+
+    const repository = repositories.find( repo => repo.id === id );
+
+    if(!repository){
+      return response.status(400).json({ error: 'Repository not found.' });
+    }
+
+    repository.likes  += 1;
+
+    return response.json(repository);
 });
 
 module.exports = app;
